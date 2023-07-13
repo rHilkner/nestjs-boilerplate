dfbed0ca560eb6fe4e6854fd9a07210ffd6b089d
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2023-07-13 17:24:32 -0300

    add prisma and generate base controllers and services from nest-cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CurrenciesModule } from './currencies/currencies.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CurrenciesModule],
   controllers: [AppController],
   providers: [AppService],
 })
