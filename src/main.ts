23798671122df4cb8501e3850694e25e26c148b4
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2024-10-12 03:22:48 -0300

    app starts successfully

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
 import { AuthorizationGuard } from './base/guards/authorization.guard'
-import { ValidationPipe } from '@nestjs/common'
 import * as dotenv from 'dotenv'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 import { env_vars } from './base/env_vars'
@@ -36,9 +35,7 @@ async function bootstrap() {
     new AuthenticationInterceptor(),
   );
 
-
   app.useGlobalGuards(new AuthorizationGuard(new Reflector()))
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 
   const port = env_vars.PORT
   await app.listen(port, '0.0.0.0');
