76d36f4c761b690378120008da064eaf3d833dcd
Rodrigo Hilkner <rodrigo.hilkner@gmail.com>
2024-10-11 21:01:48 -0300

    fixes on interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { NestFactory, Reflector } from '@nestjs/core'
 import { AppModule } from './modules/app/app.module'
-import { ApiSessionInterceptor } from './base/interceptors/api-session.interceptor'
-import { CallLogService } from './modules/call-log/call-log.service'
-import { UserService } from './modules/users/user.service'
-import { CallLogInterceptor } from './base/interceptors/call-log.interceptor'
-import { ApiSessionService } from './modules/api-session/api-session.service'
-import { ExceptionHandlerFilter } from './base/interceptors/exception-handler-filter'
 import { AuthorizationGuard } from './base/guards/authorization.guard'
-import { ErrorLogService } from './modules/error-log/error-log.service'
 import { ValidationPipe } from '@nestjs/common'
 import * as dotenv from 'dotenv'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import { env_vars } from './base/env_vars'
 
 dotenv.config()
 
@@ -21,20 +15,11 @@ async function bootstrap() {
   )
   app.enableCors()
 
-  const apiSessionService = app.get(ApiSessionService)
-  const sysCallLogService = app.get(CallLogService)
-  const errorLogService = app.get(ErrorLogService)
-  const userService = app.get(UserService)
-
   app.useGlobalGuards(new AuthorizationGuard(new Reflector()))
-  app.useGlobalFilters(new ExceptionHandlerFilter(errorLogService))
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
-  app.useGlobalInterceptors(
-    new ApiSessionInterceptor(apiSessionService, userService),
-    new CallLogInterceptor(sysCallLogService),
-  )
 
-  await app.listen(9000)
+  const port = env_vars.PORT
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap()
